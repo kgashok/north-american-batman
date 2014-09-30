ec34f72de5bbabc8c553eace7e8504d55d7af407
ashok baktha <ashok@server>
2014-09-30 14:22:11 +0530

    First error message that needs to be fixed   - Before you write code, you need a failing test   - Before you have a failing test, you need a test   - First requirement is to create a test, that also fails

--- primeF/primeF/PrimeFactorsTest.java
+++ primeF/primeF/PrimeFactorsTest.java
@@ -4,4 +4,7 @@ import junit.framework.TestCase;
 
 public class PrimeFactorsTest extends TestCase {
 
+	/* 
+	 *  junit.framework.AssertionFailedError: No tests found in primeF.PrimeFactorsTest
+	 */
 }
