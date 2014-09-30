875e4340e25fd450e7816c06cc1182aef82be93b
ashok baktha <ashok@server>
2014-09-30 14:33:10 +0530

    First test created; does not compile and that is okay

--- primeF/primeF/PrimeFactorsTest.java
+++ primeF/primeF/PrimeFactorsTest.java
@@ -7,4 +7,14 @@ public class PrimeFactorsTest extends TestCase {
 	/* 
 	 *  junit.framework.AssertionFailedError: No tests found in primeF.PrimeFactorsTest
 	 */
+	
+	/*
+	 * java.lang.Error: Unresolved compilation problems: 
+		The method list() is undefined for the type PrimeFactorsTest
+		The method generate(int) is undefined for the type PrimeFactorsTest
+	*/
+
+	public void testOne() throws Exception {
+		assertEquals(list(),generate(1));
+	}
 }
