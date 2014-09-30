482495785e81a1a7c1b936834a53611c23710eee
ashok baktha <ashok@server>
2014-09-30 15:50:57 +0530

    Test 2 as per Bob Kata passes!

--- primeF/primeF/PrimeFactors.java
+++ primeF/primeF/PrimeFactors.java
@@ -4,8 +4,13 @@ import java.util.*;
 
 public class PrimeFactors {
 
-	public static List<Integer> generate(int i) {
-		return new ArrayList<Integer>();
+	public static List<Integer> generate(int n) {
+		List<Integer> primes =  new ArrayList<Integer>();
+		if (n > 1) {
+			primes.add(2);
+		}
+		return primes;
+			
 	}
 
 }
